17f8319ca8429d838b39b331e137c35125d675a6
Freszuu_ <113879733+Freszuu7074@users.noreply.github.com>
2023-02-27 09:33:09 +0100

    Add files via upload

---
+++ 27.02.23/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        class Osoba
+        {
+            public string imie { get; set; }
+            public string naziwsko { get; set; }
+            public string stanowisko { get ; set; }
+        }
+        class Kierownik : Osoba
+        {
+            public void zlec_usluge()
+            {
+                Console.WriteLine();
+            }
+            public void zamow_czesc()
+            {
+                Console.WriteLine();
+            }
+            public void konserwuj_maszyne(Maszyna maszyna)
+            {
+                Console.WriteLine();
+                maszyna.stan_techniczny = true;
+            }
+        }
+        class Spawacz : Osoba
+        {
+            public void stworz_product()
+            {
+                Console.WriteLine();
+            }
+        }
+        class Zasob
+        {
+            public string nazwa_zasobu;
+            public int ilosc_zasobu;
+        }
+        class Czesci : Zasob
+        {
+            public string nazwa_czesci;
+            public int ilosc_czesci;
+        }
+        class Maszyna : Zasob
+        {
+            public bool stan_techniczny = false;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
