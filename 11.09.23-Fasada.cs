eab71abddaeef33c821d26d60a0812c8c6d0cda9
Freszuu_ <113879733+Freszuu7074@users.noreply.github.com>
2023-09-11 12:16:23 +0200

    Add files via upload

---
+++ 11.09.23-Fasada.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class SprawdzDostepnoscLoginu 
+    {
+        public void sprawdz()
+        {
+            Console.WriteLine("Login dostępny");
+        }
+    }
+    class DodanieDoBazy 
+    {
+        public void dodajdobazy()
+        {
+            Console.WriteLine("Dodano do bazy");
+        }
+    }
+    class DodanieDoRejestruCO2
+    {
+        public void dodajdorejestru()
+        {
+            Console.WriteLine("Dodano do rejestru CO2");
+        }
+    }
+    class WpiszDoNewslettera
+    {
+        public void wpisz()
+        {
+            Console.WriteLine("Wpisano do newslettera");
+        }
+    }
+    class Rejestracja
+    {
+        string login, haslo;
+        public static void rejestruj()
+        {
+            SprawdzDostepnoscLoginu sprawdz = new SprawdzDostepnoscLoginu();
+            DodanieDoBazy dodajdobazy = new DodanieDoBazy();
+            DodanieDoRejestruCO2 dodajdorejestru = new DodanieDoRejestruCO2();
+            WpiszDoNewslettera wpisz = new WpiszDoNewslettera();
+            sprawdz.sprawdz();
+            dodajdobazy.dodajdobazy();
+            dodajdorejestru.dodajdorejestru();
+            wpisz.wpisz();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rejestracja.rejestruj();
+            Console.ReadKey();
+        }
+    }
+}
